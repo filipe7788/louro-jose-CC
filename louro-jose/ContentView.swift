6957573a7b580075aacd3f4397d966a6f73a2255
Filipe C <filipe.cruz.ribeiro@gmail.com>
2020-12-04 09:09:01 -0300

    Initial Commit

---
+++ louro-jose/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  louro-jose
+//
+//  Created by Filipe Cruz on 04/12/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
