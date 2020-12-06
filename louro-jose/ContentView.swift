02a6a61234ae206a92393cf9bbeb419de186841b
Filipe Cruz <filipe.cruz.ribeiro@gmail.com>
2020-12-05 23:56:27 -0300

    sharing feature

--- louro-jose/ContentView.swift
+++ louro-jose/ContentView.swift
@@ -10,11 +10,6 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         Home()
-    }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+            .background(Color.white)
     }
 }
--- louro-jose/Views/Home.swift
+++ louro-jose/Views/Home.swift
@@ -59,20 +59,29 @@ struct Home: View {
         }.tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
         
         .sheet(isPresented: $isSharedSheetShowing) {
-            ShareSheet(itens: [])
+            ShareSheet(activityItems: itens)
         }
     }
 }
 
 struct ShareSheet: UIViewControllerRepresentable {
-    var itens: [Any]
+    typealias Callback = (_ activityType: UIActivity.ActivityType?, _ completed: Bool, _ returnedItems: [Any]?, _ error: Error?) -> Void
     
-    func makeUIViewController(context: Context) ->  UIActivityViewController {
-        let controller = UIActivityViewController(activityItems: itens, applicationActivities: nil)
+    let activityItems: [Any]
+    let applicationActivities: [UIActivity]? = nil
+    let excludedActivityTypes: [UIActivity.ActivityType]? = nil
+    let callback: Callback? = nil
+    
+    func makeUIViewController(context: Context) -> UIActivityViewController {
+        let controller = UIActivityViewController(
+            activityItems: activityItems,
+            applicationActivities: applicationActivities)
+        controller.excludedActivityTypes = excludedActivityTypes
+        controller.completionWithItemsHandler = callback
         return controller
     }
     
     func updateUIViewController(_ uiViewController: UIActivityViewController, context: Context) {
-        
+        // nothing to do here
     }
 }
